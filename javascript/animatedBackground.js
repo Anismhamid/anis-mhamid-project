8f8c5e1995b3af674cc74cf3b6648163117b427a
Anis Mhamid <anesmhamed1@gmail.com>
2024-06-01 19:35:59 +0300

    upgrades

--- javascript/animatedBackground.js
+++ javascript/animatedBackground.js
@@ -9,7 +9,7 @@ container.onmousemove = function (Event) {
     layer[1].style.transform = "translate(" + X / 100 * 2 + 'px, ' + Y / 100 * 2 + 'px)'
     layer[2].style.transform = "translate(" + X / 100 * 4 + 'px, ' + Y / 100 * 4 + 'px)'
     layer[3].style.transform = "translate(" + X / 100 * 6 + 'px, ' + Y / 100 * 6 + 'px)'
-    layer[4].style.transform = "translate(" + X / 50 * 8 + 'px, ' + Y / 100 * 12 + 'px)'
+    layer[4].style.transform = "translate(" + X / 70 * 8 + 'px, ' + Y / 100 * 12 + 'px)'
     layer[5].style.transform = "translate(" + X / 100 * 10 + 'px, ' + Y / 100 * 10 + 'px)'
-    layer[6].style.transform = "translate(" + X / 100 * 2 + 'px, ' + Y / 60 * 20 + 'px)'
+    layer[6].style.transform = "translate(" + X / 100 * 2 + 'px, ' + Y / 60 * 30 + 'px)'
 }
