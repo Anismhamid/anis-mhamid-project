eb06b0ad1c002035e13efe9dbb4195ea3b74351a
Anis Mhamid <anesmhamed1@gmail.com>
2024-08-07 21:49:52 +0300

    complated

---
+++ javascript/oriented.js
@@ -0,0 +1,250 @@
+class Project {
+    constructor(title, subTitle, image, description, pageUrl) {
+        this.title = title;
+        this.subTitle = subTitle;
+        this.image = image;
+        this.description = description;
+        this.pageUrl = pageUrl;
+    }
+}
+
+
+let htmlCssProjects = [
+    {
+        id: 1,
+        title: "תן לנו להוביל אותך קדימה",
+        subTitle: "",
+        image: "./img/pages-images/takeTheFuture.png",
+        description: "הדף מספק חווית משתמש אטרקטיבית וקלה לשימוש במכשירים שונים, שכן הוא משקף הקפדה על פרטים ועיצוב משוכלל המשלב אלגנטיות ופונקציונליות יעילה, המבטיח אינטראקציה חלקה ונוחה למשתמשים עם התוכן.",
+        pageUrl: "./pages/future.html",
+        alt: "anis mhamid let us lead you forward",
+    },
+    {
+        id: 2,
+        title: "כאן ועכשיו",
+        subTitle: "",
+        image: "./img/pages-images/hereAndNow.png",
+        description: "עמוד זה מאופיין ביופיו האלגנטי. העמוד משדר אווירה נהדרת. העיצוב הנקי והמאורגן שלו הופך את חווית הגלישה למהנה ונוחה למשתמשים. הטקסטים הקיימים מנחים את המבקרים בעדינות וקלים להבנה, גורמים להם להרגיש רצוי ומבורכים. בנוסף, התמונה הכוללת של העמוד משקפת משיכה מיוחדת שגורמת למבקרים לרצות לחקור יותר מהתוכן שלו ולנצל את ההצעות הזמינות. בסך הכל, לדף יש אסתטיקה בולטת שמושכת את העין והופכת את החוויה המקוונת למשתמשים למהנה ומרגשת.",
+        pageUrl: "./pages/here-and-now.html",
+        alt: "anis mhamid here and now"
+    },
+    {
+        id: 3,
+        title: "ייעוץ חינם",
+        subTitle: "",
+        image: "./img/pages-images/free-consultation-porject5-image2.png",
+        description: 'דף רספונסיבי שמשמש להצגת שירות "ייעוץ חינם" למשתמשים. מופיע כותרת "ייעוץ חינם" עם תיאור קצר של השירות.קיים טופס למילוי פרטים אישיים כגון שם מלא, דוא"ל ופרטי הפניה. קיימות גם סמלים חברתיים ליצירת קשר ומספר טלפון ליצירת קשר ישיר.הדף מכיל עיצוב רספונסיבי שמתאים לגדלי מסכים ולתצורות שונות של התקנים',
+        pageUrl: "./pages/free-consultation-page.html",
+        alt: "anis mhamid free-consultation-page"
+    },
+    {
+        id: 4,
+        title: "תן לנו להוביל אותך קדימה",
+        subTitle: "",
+        image: "./img/pages-images/leading-forward.png",
+        description: 'עמוד זה מאופיין ביופיו האלגנטי. העמוד משדר אווירה נהדרת. העיצוב הנקי והמאורגן שלו הופך את חווית הגלישה למהנה ונוחה למשתמשים. הטקסטים הקיימים מנחים את המבקרים בעדינות וקלים להבנה, גורמים להם להרגיש רצוי ומבורכים. בנוסף, התמונה הכוללת של העמוד משקפת משיכה מיוחדת שגורמת למבקרים לרצות לחקור יותר מהתוכן שלו ולנצל את ההצעות הזמינות. בסך הכל, לדף יש אסתטיקה בולטת שמושכת את העין והופכת את החוויה המקוונת למשתמשים למהנה ומרגשת',
+        pageUrl: "./pages/leading-forward.html",
+        alt: "anis mhamid Free Consultation Page"
+    },
+    {
+        id: 5,
+        title: "קפה טוב לבוקר",
+        subTitle: "",
+        image: "./img/pages-images/morning-coffe.jpg",
+        description: 'העמוד תוכנן בצורה שמאפשרת חוויית משתמש חלקה ונעימה לכל סוגי המסכים.בעת פתיחת העמוד במסך גדול, האיזור הימני מוקדש לתמונה גדולה ומרשימה של הקפה, יחד עם טופס ההרשמה המצורף',
+        pageUrl: "./pages/morning-coffe-page.html",
+        alt: "anis mhamid Morning Coffe"
+    },
+    {
+        id: 6,
+        title: "תראה את העולם מקרוב",
+        subTitle: "",
+        image: "./img/pages-images/see-the-wold.jpg",
+        description: 'העמוד תוכנן בצורה שמאפשרת חוויית משתמש חלקה ונעימה לכל סוגי המסכים.בעת פתיחת העמוד במסך גדול, האיזור הימני מוקדש לתמונה גדולה ומרשימה של הקפה, יחד עם טופס ההרשמה המצורף',
+        pageUrl: "./pages/see-the-wold-page.html",
+        alt: "anis mhamid See The Wold"
+    },
+    {
+        id: 7,
+        title: "לחשוב מחוץ לקופסה",
+        subTitle: "",
+        image: "./img/pages-images/think-so-far.png",
+        description: 'הדף רספונסיבי למכשירים שונים, מה שאומר שעיצובו מתאים לגדלי מסכים שונים ולסוגי מכשירים שונים, ממחשבים שולחניים עד הטלפונים הניידים.העיצוב הזה מקל על חוויית המשתמש, מספק נוחות וקלות למשתמשים במגוון מכשירים, ומבטיח חוויית משתמש חלקה ונעימה',
+        pageUrl: "./pages/think-so-far-page.html",
+        alt: "anis mhamid Think Far"
+    },
+    {
+        id: 8,
+        title: "תן לנו לבנות את בית החלומות שלך",
+        subTitle: "",
+        image: "./img/dream-home.png",
+        description: ' הדף רספונסיבי למכשירים שונים, מה שאומר שעיצובו מתאים לגדלי מסכים שונים ולסוגי מכשירים שונים, ממחשבים שולחניים עד הטלפונים הניידים.העיצוב הזה מקל על חוויית המשתמש',
+        pageUrl: "/pages/dream-home.html",
+        alt: "anis mhamid Dream Home"
+    },
+    {
+        id: 9,
+        title: "עולם האופנה שלנו",
+        image: "./img/pages-images/sashion.jpg",
+        alt: "anis mhamid Fashion World",
+        description: ' הדף מספק חווית משתמש אטרקטיבית וקלה לשימוש במכשירים שונים',
+        pageUrl: "",
+        subTitle: "",
+    },
+]
+for (let htmlProject of htmlCssProjects) {
+    console.log(htmlProject.title);
+    document.getElementById("row1").innerHTML += `
+        <!-- 1 -->
+        <div class="col-sm-12 m-auto col-lg-3 my-4">
+            <div class="parent">
+                <div class="card" style="width: 20rem;">
+                    <div class="content-box">
+                        <span class="card-title">${htmlProject.title}</span>
+                        <div class="card-img">
+                            <img src="${htmlProject.image}" class="card-img-top"
+                                alt="${htmlProject.alt}">
+                        </div>
+                        <button type="button" class="see-more btn btn-outline-primary" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal${htmlProject.id}">
+                            פרטים נוספים
+                        </button>
+                    </div>
+                    <div class="date-box">
+                        <span class="month">April</span>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal${htmlProject.id}" tabindex="-1" aria-labelledby="exampleModalLabel${htmlProject.id}"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel${htmlProject.id}">${htmlProject.title}</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p class="lead pb-4">
+                                ${htmlProject.description}
+                            </p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">סגירה</button>
+                            <a href="${htmlProject.pageUrl}" target="_blank">
+                                <button type="button" class="btn btn-outline-primary">לעמוד הפרויקט</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+`
+    console.log(new Project(htmlProject.title))
+}
+
+let javaScriptProjects = [
+    {
+        id: 10,
+        title: "מחשבון",
+        image: "./img/Calculator.png",
+        alt: "anis mhamid Calculator",
+        description: "HTML לבניית מבנה העמוד והחזית Bootstrap כדי לשפר את עיצוב הממשק ולהפוך אותו למגיב. JavaScript ליישום היגיון תכנות וחישובים",
+        pageUrl: "./pages/calculator.html",
+    },
+    {
+        id: 11,
+        title: "Flappy Bird game",
+        image: "./img/FlappyBird.png",
+        alt: "anis mhamid Flappy Bird game",
+        description: "משחק רספונסיבי למכשירים שונים, מה שאומר שעיצובו מתאים לגדלי מסכים שונים ולסוגי מכשירים שונים, ממחשבים שולחניים עד הטלפונים הניידים, ",
+        pageUrl: "./pages/flappyBird.html",
+    },
+    {
+        id: 12,
+        title: "עגלת קניו",
+        image: "./img/products.png",
+        alt: "anis mhamid products page",
+        description: "הדף מספק חווית משתמש אטרקטיבית וקלה לשימוש במכשירים שונים",
+        pageUrl: "./pages/products-cart.html",
+    },
+    {
+        id: 13,
+        title: "Ultimata Tic Tac Toe",
+        image: "./img/ultimate-TicTacToe.png",
+        alt: "anis mhamid X O Game",
+        description: "המשחק מספק חווית משתמש אטרקטיבית וקלה לשימוש במכשירים שונים",
+        pageUrl: "./pages/x-O.html",
+    },
+    {
+        id: 14,
+        title: "Snake",
+        image: "./img/dode.png",
+        alt: "anis mhamid Snake Game",
+        description: "המשחק מספק חווית משתמש אטרקטיבית וקלה לשימוש במכשירים שונים",
+        pageUrl: "./pages/snake.html",
+    },
+    {
+        id: 15,
+        title: "Manage U",
+        image: "./img/manageU2.png",
+        alt: "anis mhamid Snake Game",
+        description: "המשחק מספק חווית משתמש אטרקטיבית וקלה לשימוש במכשירים שונים",
+        pageUrl: "./pages/manageU.html",
+    },
+
+]
+
+for (let jSProject of javaScriptProjects) {
+    document.getElementById("row2").innerHTML += `
+        <div class="col-sm-12 m-auto col-lg-3 my-4">
+            <div class="parent">
+                <div class="card" style="width: 20rem;">
+                    <div class="content-box">
+                        <span class="card-title">${jSProject.title}</span>
+                        <div class="card-img">
+                            <img src="${jSProject.image}" class="card-img-top"
+                                alt="${jSProject.alt}">
+                        </div>
+                        <button type="button" class="see-more btn btn-outline-primary" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal${jSProject.id}">
+                            פרטים נוספים
+                        </button>
+                    </div>
+                    <div class="date-box">
+                        <span class="month">April</span>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal${jSProject.id}" tabindex="-1" aria-labelledby="exampleModalLabel${jSProject.id}"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel${jSProject.id}">${jSProject.title}</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p class="lead pb-4">
+                                ${jSProject.description}
+                            </p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">סגירה</button>
+                            <a href="${jSProject.pageUrl}" target="_blank">
+                                <button type="button" class="btn btn-outline-primary">לעמוד הפרויקט</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+`
+}
+
+
