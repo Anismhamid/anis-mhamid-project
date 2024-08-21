3277a989e4ab1033d0419113f93cd36c52584ad0
Anis Mhamid <anesmhamed1@gmail.com>
2024-08-21 22:07:13 +0300

    updates

---
+++ projects/js/xO/oop1.js
@@ -0,0 +1,9 @@
+class Game{
+    constructor( currentPlayer,gamesCounter,xCounter,oCounter,){
+        this.currentPlayer = currentPlayer
+        this.gamesCounter = gamesCounter
+        this.xCounter = xCounter;
+        this.oCounter = oCounter
+    }
+}
+
